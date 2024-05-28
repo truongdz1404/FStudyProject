be2e2fcc7c0dfb27ddaf1af4a88403854d75381e
idk004-ai <KhoiLNMHE180029@fpt.edu.vn>
2024-05-29 03:36:37 +0700

    Demo Login

--- FStudyForum.Client/src/App.tsx
+++ FStudyForum.Client/src/App.tsx
@@ -2,13 +2,17 @@ import AuthProvider from "./contexts/auth/AuthContext";
 import { CLIENT_ID } from "./helpers/constants";
 import Router from "./router";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { ToastContainer } from "react-toastify";
 
 export default function App() {
     return (
-        <GoogleOAuthProvider clientId={CLIENT_ID}>
-            <AuthProvider>
-                <Router />
-            </AuthProvider>
-        </GoogleOAuthProvider>
+        <>
+            <ToastContainer />
+            <GoogleOAuthProvider clientId={CLIENT_ID}>
+                <AuthProvider>
+                    <Router />
+                </AuthProvider>
+            </GoogleOAuthProvider>
+        </>
     );
 }
