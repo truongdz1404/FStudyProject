9e3b00c9c3455c98f6b4ceb3cf7638aba0b4c0c8
bacnxhe172646 <bacnxhe172646@fpt.edu.vn>
2024-06-03 23:01:06 +0700

    Fix logout

--- FStudyForum.Client/src/pages/auth/signout/index.tsx
+++ FStudyForum.Client/src/pages/auth/signout/index.tsx
@@ -1,30 +1,22 @@
 import { signOut } from "@/contexts/auth/reduce";
 import { useAuth } from "@/hooks/useAuth";
 import AuthService from "@/services/AuthService";
-import { FC } from "react";
+import React, { FC } from "react";
 import { useNavigate } from "react-router-dom";
 
 const SignOut: FC = () => {
-     const { dispatch } = useAuth();
-     const navigate = useNavigate(); 
-     const handleLogout = async () => {
-          try{
-               await AuthService.logout();
-               dispatch(signOut());
-               navigate("/auth/signin");
-          }catch (error){
-               console.log(error);
-          }
-     }
-     return(
-          <div className="dark:bg-orange-500 rounded-lg">
-               <button 
-                    onClick={handleLogout}
-                    className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"
-               >
-                    Logout
-               </button>
-          </div>
-     )
-}
+  const { dispatch } = useAuth();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    const signout = async () => {
+      await AuthService.logout();
+      dispatch(signOut());
+    };
+    signout();
+    navigate("/auth/signin");
+  }, [navigate, dispatch]);
+
+  return <></>;
+};
 export default SignOut;
