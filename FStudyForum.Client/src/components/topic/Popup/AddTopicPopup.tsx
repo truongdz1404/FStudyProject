189c97dbc7bc760a503959a78657695b4ec97d8d
@vietcdhe153298 <vietcdhe153298@fpt.edu.vn>
2024-06-22 16:04:50 +0700

    Fix Add topic

--- FStudyForum.Client/src/components/topic/Popup/AddTopicPopup.tsx
+++ FStudyForum.Client/src/components/topic/Popup/AddTopicPopup.tsx
@@ -1,133 +1,298 @@
-import React, { useState, useEffect } from "react"
-import { Alert } from "@material-tailwind/react"
-import { CreateTopicDTO } from "@/types/topic"
-import TopicService from "@/services/TopicService"
+import React, { useState, useEffect } from "react";
+import { Alert, Chip, Input, Step, Stepper } from "@material-tailwind/react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import * as Yup from "yup";
+import { ChevronLeft, ChevronRight, CircleAlert } from "lucide-react";
+import { motion } from "framer-motion";
+import { Category } from "@/types/category"; 
+import TopicService from "@/services/TopicService";
+import CategoryService from "@/services/CategoryService";
+import Success from "@/assets/images/motivation.gif";
+import { X } from "lucide-react"; 
 
 interface AddTopicPopupProps {
-  onClose: () => void
-  onTopicCreated: () => void
+  onClose: () => void;
+  onTopicCreated: () => void;
 }
+
+interface AddTopicFormInputs {
+  topicName: string;
+  description: string;
+}
+
+const validation = Yup.object().shape({
+  topicName: Yup.string()
+    .trim()
+    .required("Topic name is required")
+    .max(50, "Topic name must be no more than 50 characters"),
+  description: Yup.string().trim().required("Description is required")
+});
 
 const AddTopicPopup: React.FC<AddTopicPopupProps> = ({
   onClose,
   onTopicCreated
 }) => {
-  const [topicName, setTopicName] = useState("")
-  const [description, setDescription] = useState("")
-  const [selectedCategories, setSelectedCategories] = useState<string[]>([])
-  const [error, setError] = useState<string | null>(null)
-  const [successMessage, setSuccessMessage] = useState<string | null>(null)
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [selectedCategories, setSelectedCategories] = useState<number[]>([]);
+  const [activeStep, setActiveStep] = useState(0);
+  const [preStep, setPreStep] = useState(-1);
+  const [isLastStep, setIsLastStep] = useState(false);
+  const [isFirstStep, setIsFirstStep] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
-  const handleAddTopic = async () => {
-    try {
-      const newTopic: CreateTopicDTO = {
-        name: topicName,
-        description,
-        categories: selectedCategories.map(category => parseInt(category))
-      }
-
-      await TopicService.create(newTopic)
-      setTopicName("")
-      setDescription("")
-      setSelectedCategories([])
-      onClose()
-      onTopicCreated()
-    } catch (error) {
-      console.error("Error creating topic:", error)
-      setError("Error creating topic. Please try again later.")
-    }
-  }
+  const {
+    register,
+    handleSubmit,
+    trigger,
+    formState: { errors }
+  } = useForm<AddTopicFormInputs>({
+    mode: "onTouched",
+    resolver: yupResolver(validation)
+  });
 
   useEffect(() => {
-    if (successMessage) {
-      const timeout = setTimeout(() => {
-        setSuccessMessage(null)
-      }, 3000)
+    (async () => {
+      try {
+        setLoading(true);
+        const fetchedCategories = await CategoryService.getAllCategory();
+        setCategories(fetchedCategories);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, []);
 
-      return () => clearTimeout(timeout)
+  const handleNext = async () => {
+    let isValid = false;
+    switch (activeStep) {
+      case 0:
+        isValid = await trigger(["topicName", "description"]);
+        break;
+      default:
+        isValid = true;
+        break;
     }
-  }, [successMessage])
+    if (isValid) {
+      nextStep();
+    }
+  };
+
+  const handleAddTopic = async (data: AddTopicFormInputs) => {
+    try {
+      setLoading(true);
+      const newTopic = {
+        name: data.topicName,
+        description: data.description,
+        categories: selectedCategories
+      };
+      await TopicService.create(newTopic);
+      onClose();
+      onTopicCreated();
+    } catch (error) {
+      console.error("Error creating topic:", error);
+      setError("Error creating topic. Please try again later.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const nextStep = () => {
+    if (!isLastStep) {
+      setPreStep(activeStep);
+      setActiveStep((cur) => cur + 1);
+    }
+  };
+
+  const prevStep = () => {
+    if (!isFirstStep) {
+      setPreStep(activeStep);
+      setActiveStep((cur) => cur - 1);
+    }
+  };
+
+  const handleCategoryChange = (categoryId: number) => {
+    setSelectedCategories((prevCategories) =>
+      prevCategories.includes(categoryId)
+        ? prevCategories.filter((id) => id !== categoryId)
+        : [...prevCategories, categoryId]
+    );
+  };
+
+  const renderStep = (step: number) => {
+    return (
+      <>
+        <div className={step !== 0 ? "hidden" : ""}>
+          <div className="mb-6">
+            <p className="text-xl font-semibold">Topic Information</p>
+            <p className="text-xs text-gray-600 text-left">
+              Provide details about the topic you want to create
+            </p>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="topicName" className="block font-bold mb-1">
+              Topic Name
+            </label>
+            <Input
+              crossOrigin={undefined} id="topicName"
+              type="text"
+              placeholder="Enter topic name"
+              className="border border-gray-300 rounded px-2 py-1 w-full"
+              {...register("topicName")}
+              error={Boolean(errors.topicName?.message)}            />
+            {errors.topicName && (
+              <span className="text-red-500 text-xs mt-1">
+                <CircleAlert className="w-3 h-3" /> {errors.topicName.message}
+              </span>
+            )}
+          </div>
+          <div className="mb-4">
+            <label htmlFor="description" className="block font-bold mb-1">
+              Description
+            </label>
+            <textarea
+              id="description"
+              placeholder="Enter description"
+              className="border border-gray-300 rounded px-2 py-1 w-full h-24 resize-none"
+              {...register("description")}
+            ></textarea>
+            {errors.description && (
+              <span className="text-red-500 text-xs mt-1">
+                <CircleAlert className="w-3 h-3" /> {errors.description.message}
+              </span>
+            )}
+          </div>
+        </div>
+        <div className={step !== 1 ? "hidden" : ""}>
+          <div className="mb-6">
+            <p className="text-xl font-semibold">Select Categories</p>
+            <p className="text-xs text-gray-600 text-left">
+              Choose categories that fit your topic
+            </p>
+            <div className="overflow-x-auto max-h-[20rem]">
+              <div className="mt-6 w-full flex flex-wrap gap-2">
+                {categories.map((category) => (
+                  <div
+                    key={category.id}
+                    onClick={() => handleCategoryChange(category.id)}
+                    className="hover:cursor-pointer"
+                  >
+                    <Chip
+                      color="blue-gray"
+                      variant={
+                        selectedCategories.includes(category.id)
+                          ? "filled"
+                          : "ghost"
+                      }
+                      value={category.name}
+                      className="rounded-full capitalize"
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={step !== 2 ? "hidden" : ""}>
+          <div className="mt-6 flex flex-col items-center justify-center">
+            <img className="w-20 h-20" src={Success} alt="Success" />
+            <p className="text-lg font-semibold text-blue-gray-800">
+              Your topic has been successfully created!
+            </p>
+          </div>
+        </div>
+      </>
+    );
+  };
 
   return (
     <div className="fixed top-0 left-0 w-full h-full bg-gray-700 bg-opacity-50 flex justify-center items-center">
-      <div className="bg-white p-8 rounded-md w-96">
+      <form
+        className="bg-white p-8 rounded-md w-96 relative" 
+        onSubmit={handleSubmit(handleAddTopic)}
+      >
+        <button
+          type="button"
+          onClick={onClose} 
+          className="absolute top-2 right-2 p-2 rounded-full bg-gray-200 hover:bg-gray-300"
+        >
+          <X className="w-6 h-6 text-gray-600" /> 
+        </button>
         <h2 className="text-xl font-bold mb-4">Add Topic</h2>
         {error && (
           <Alert color="red" className="mb-4">
             {error}
           </Alert>
         )}
-        {successMessage && (
-          <Alert color="green" className="mb-4">
-            {successMessage}
-          </Alert>
-        )}
-        <div className="mb-4">
-          <label htmlFor="topicName" className="block font-bold mb-1">
-            Topic Name:
-          </label>
-          <input
-            type="text"
-            id="topicName"
-            value={topicName}
-            onChange={e => setTopicName(e.target.value)}
-            className="border border-gray-300 rounded px-2 py-1 w-full"
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="description" className="block font-bold mb-1">
-            Description:
-          </label>
-          <textarea
-            id="description"
-            value={description}
-            onChange={e => setDescription(e.target.value)}
-            className="border border-gray-300 rounded px-2 py-1 w-full h-24 resize-none"
-          ></textarea>
-        </div>
-        <div className="mb-4">
-          <label htmlFor="categories" className="block font-bold mb-1">
-            Categories:
-          </label>
-          <select
-            id="categories"
-            value={selectedCategories}
-            onChange={e =>
-              setSelectedCategories(
-                Array.from(e.target.selectedOptions, option => option.value)
-              )
-            }
-            className="border border-gray-300 rounded px-2 py-1 w-full"
+        <Stepper
+          activeStep={activeStep}
+          isLastStep={(value) => setIsLastStep(value)}
+          isFirstStep={(value) => setIsFirstStep(value)}
+          lineClassName="bg-orange-200"
+          activeLineClassName="bg-deep-orange-400"
+          className="mb-4"
+        >
+          <Step
+            className="!bg-orange-300 !text-white w-8 h-8"
+            activeClassName="!bg-deep-orange-400"
+            completedClassName="!bg-deep-orange-400"
           >
-            <option value="1">Semester 1</option>
-            <option value="2">Semester 2</option>
-            <option value="3">Semester 3</option>
-            <option value="4">Semester 4</option>
-            <option value="5">Semester 5</option>
-            <option value="6">Semester 6</option>
-            <option value="7">Semester 7</option>
-            <option value="8">Semester 8</option>
-            <option value="9">Semester 9</option>
-            <option value="10">Semester 10</option>
-          </select>
-        </div>
-        <div className="flex justify-between">
-          <button
-            onClick={handleAddTopic}
-            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-          >
-            Add Topic
-          </button>
-          <button
-            onClick={onClose}
-            className="bg-gray-300 text-gray-700 px-4 py-2 rounded hover:bg-gray-400"
-          >
-            Close
-          </button>
-        </div>
-      </div>
-    </div>
-  )
-}
+            1
+          </Step> 
+          <Step
+            className="!bg-orange-300 !text-white w-8 h-8"
+           
 
-export default AddTopicPopup
+            activeClassName="!bg-deep-orange-400"
+            completedClassName="!bg-deep-orange-400"
+          >
+            2
+          </Step>
+          <Step
+            className="!bg-orange-300 !text-white w-8 h-8"
+            activeClassName="!bg-deep-orange-400"
+            completedClassName="!bg-deep-orange-400"
+          >
+            3
+          </Step>
+        </Stepper>
+        <motion.div
+          key={activeStep}
+          initial={{ x: preStep < activeStep ? -2 : 2, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ ease: "easeInOut", duration: 0.5 }}
+        >
+          {renderStep(activeStep)}
+        </motion.div>
+        <div className="flex justify-between mt-6">
+          <button
+            type="button"
+            onClick={() => {
+              !loading && prevStep();
+            }}
+            className={`flex gap-x-2 text-deep-orange-400 hover:cursor-pointer select-none font-bold ${
+              isFirstStep ? "text-orange-100" : ""
+            }`}
+          >
+            <ChevronLeft /> Back
+          </button>
+          <button
+            type={isLastStep ? "submit" : "button"}
+            onClick={() => {
+              !loading && !isLastStep && handleNext();
+            }}
+            className="flex gap-x-2 text-deep-orange-400 hover:cursor-pointer select-none font-bold"
+          >
+            {!isLastStep ? "Next" : "Finish"}
+            <ChevronRight />
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddTopicPopup;
--- FStudyForum.Client/src/types/topic.d.ts
+++ FStudyForum.Client/src/types/topic.d.ts
@@ -12,19 +12,11 @@ export interface Topic {
 export interface CreateTopicDTO {
   name: string;
   description: string;
-  avatar: string;
-  banner: string;
-  postCount: number;
-  isDeleted: boolean;
   categories: number[];
 }
 
 export interface UpdateTopicDTO {
   name: string;
   description: string;
-  avatar: string;
-  banner: string;
-  postCount: number;
-  isDeleted: boolean;
   categories: number[];
 }
